bd4d4954a03b1a280504c5fe5220d60cd34e6f9f
Etin Obaseki <etinosaobaseki@gmail.com>
2023-02-18 12:30:55 +0100

    feat: Add static home page

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html/**/*.{html,tmpl}", "./js/*.js"],
+  content: ["./html/**/*.html", "./js/*.js"],
   theme: {
     extend: {},
   },
