5435baa9694697fded5c4019e9f5bc33fc47588a
Etin Obaseki <etinosaobaseki@gmail.com>
2023-02-18 18:17:50 +0100

    feat: add helpers to DI
    add error handlers to app struct
    use app error handlers for errors in HTTP handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,14 +7,9 @@ import (
 	"strconv"
 )
 
-func handleError(err error, w http.ResponseWriter, app *application) {
-	app.errorLog.Print(err.Error())
-	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-}
-
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -26,13 +21,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		handleError(err, w, app)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		handleError(err, w, app)
+		app.serverError(w, err)
 		return
 	}
 }
@@ -40,7 +35,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) letterView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	fmt.Fprintf(w, "Viewing a specific letter with ID %d...", id)
@@ -49,7 +44,7 @@ func (app *application) letterView(w http.ResponseWriter, r *http.Request) {
 func (app *application) letterCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new letter..."))
