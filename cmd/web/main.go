2283995a7c5c5bcc3f738234294c4adccaa43220
Etin Obaseki <etinosaobaseki@gmail.com>
2023-02-19 11:19:57 +0100

    feat: refactor routes
    move routes to dedicated folder in application struct
    call app.routes in server instance

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,20 +24,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/letters/view/", app.letterView)
-	mux.HandleFunc("/letters/create/", app.letterCreate)
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on port %s", *addr)
