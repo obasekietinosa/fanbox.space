5aae86395b880f4586f7906768155bff26b422fb
Etin Obaseki <etinosaobaseki@gmail.com>
2023-02-19 19:42:27 +0100

    feat: Update Letters models
    Add Get and Latest methods to Letters model

--- internal/models/letters.go
+++ internal/models/letters.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -37,9 +38,43 @@ func (model *LetterModel) Insert(
 }
 
 func (model *LetterModel) Get(id int) (*Letter, error) {
-	return nil, nil
+	stmt := "SELECT id, email, subject, author, recipient, content, salutation, created from letters where id = ?"
+	row := model.DB.QueryRow(stmt, id)
+
+	letter := &Letter{}
+
+	err := row.Scan(&letter.ID, &letter.Email, &letter.Subject, &letter.Author, &letter.Recipient, &letter.Content, &letter.Salutation, &letter.Created)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		}
+		return nil, err
+	}
+	return letter, nil
 }
 
-func (model *LetterModel) Latest(limit int) (*[]Letter, error) {
-	return nil, nil
+func (model *LetterModel) Latest(limit int) ([]*Letter, error) {
+	stmt := "SELECT id, email, subject, author, recipient, content, salutation, created from letters ORDER BY id DESC LIMIT ?"
+	rows, err := model.DB.Query(stmt, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	letters := []*Letter{}
+
+	for rows.Next() {
+		letter := &Letter{}
+		err = rows.Scan(&letter.ID, &letter.Email, &letter.Subject, &letter.Author, &letter.Recipient, &letter.Content, &letter.Salutation, &letter.Created)
+		if err != nil {
+			return nil, err
+		}
+		letters = append(letters, letter)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return letters, nil
 }
