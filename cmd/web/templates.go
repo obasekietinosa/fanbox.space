ce38f612d10bfb6aa6afe114ebc2d1eaf0fbc199
Etin Obaseki <etinosaobaseki@gmail.com>
2023-09-11 20:49:09 +0100

    feat: use embedded file system for static assets

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"www.fanbox.space/internal/models"
+	"www.fanbox.space/ui"
 )
 
 type composeData struct {
@@ -32,24 +34,20 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.go.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.go.html")
 	if err != nil {
 		return nil, err
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.go.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.go.html",
+			"html/partials/*.go.html",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.go.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
